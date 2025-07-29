111df38f90734641bdb97c0f008d68506fe3e227
Bartosz Janiak <bartoszjaniak@outlook.com>
2025-07-29 19:36:44 +0200

    filtering

--- app/src/app/user-list/user-list.component.ts
+++ app/src/app/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../services/user.service';
-import { map } from 'rxjs';
+import { debounce, debounceTime, map, startWith, switchMap } from 'rxjs';
 import {
   MatCell,
   MatCellDef,
@@ -16,11 +17,15 @@ import {
 } from '@angular/material/table';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     MatTable,
@@ -33,13 +38,22 @@ import { CommonModule } from '@angular/common';
     MatCellDef,
     MatHeaderRowDef,
     MatRowDef,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
 })
 export class UserListComponent {
   private userService = inject(UserService);
 
-  protected usersDataSource$ = this.userService
-    .getUsers()
-    .pipe(map((users) => new MatTableDataSource(users)));
+  protected filter = new FormControl<string>('', { nonNullable: true });
+
+  protected usersDataSource$ = this.filter.valueChanges.pipe(
+    startWith(''),
+    debounceTime(300),
+    switchMap((filter) => this.userService.getUsers(filter)),
+    map((users) => new MatTableDataSource(users))
+  );
+
 }
